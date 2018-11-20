a9bae9fd1ec972905bcac04b8b8acf02b7300e78
unknown <pjconnolly12@gmail.com>
2018-11-19 22:40:11 -0500

    Real Python Flask Blog project

---
+++ flask-blog/bin/blog.py
@@ -0,0 +1,26 @@
+# blog.py - controller
+
+from flask import Flask, render_template, requests, session, \
+	flash, redirect, url_for, g
+import sqlite3
+
+#configiuration
+DATABASE = 'blog.db'
+
+app = Flask(__name__)
+#pulls in app configuration by looking for UPPERCASE variables
+app.config.from_object(__name__)
+#function used for connecting to the database
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+@app.route('/')
+def login():
+	return render_template('login.html')
+
+@app.route('/main')
+def main():
+	return render_template('main.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
