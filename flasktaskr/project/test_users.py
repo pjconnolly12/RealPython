30c79e922991b0617b8961b5772c2c79f9f68d74
unknown <pjconnolly12@gmail.com>
2018-12-04 21:05:32 -0500

    finished setting up the test suite

--- flasktaskr/project/test_users.py
+++ flasktaskr/project/test_users.py
@@ -9,7 +9,7 @@ from models import User
 
 TEST_DB = 'test.db'
 
-class AllTests(unittest.TestCase):
+class UserTests(unittest.TestCase):
 
 	#setup and teardown
 
@@ -67,7 +67,7 @@ class AllTests(unittest.TestCase):
 		assert t.name == "michael"
 
 	#form is present
-	def test_form_is_present(self):
+	def test_form_is_present_on_login_page(self):
 		response = self.app.get('/')
 		self.assertEqual(response.status_code, 200)
 		self.assertIn(b'Please sign in to access your task list', response.data)
@@ -123,70 +123,21 @@ class AllTests(unittest.TestCase):
 		response = self.logout()
 		self.assertNotIn(b'Goodbye!', response.data)
 
-	def test_logged_in_users_can_access_tasks_page(self):
-		self.register(
-		    'Fletcher', 'fletcher@realpython.com', 'python101', 'python101'
+	def test_default_user_role(self):
+		db.session.add(
+			User(
+				"Johnny",
+				"jon@doe.com",
+				"johnny"
+			)
 		)
-		self.login('Fletcher', 'python101')
-		response = self.app.get('tasks/')
-		self.assertEqual(response.status_code, 200)
-		self.assertIn(b'Add a new task:', response.data)
+		db.session.commit()
+		users = db.session.query(User).all()
+		print(users)
+		for user in users:
+			self.assertEquals(user.role, 'user')
+	
 
-	def test_not_logged_in_users_cannot_access_tasks_page(self):
-		response = self.app.get('tasks/', follow_redirects=True)
-		self.assertIn(b'You need to login first.', response.data)
-
-	def test_users_can_add_tasks(self):
-		self.create_user('Michael', 'michael@realpython.com', 'python')
-		self.login('Michael', 'python')
-		self.app.get('tasks/', follow_redirects=True)
-		response = self.create_task()
-		self.assertIn(
-		    b'New entry was successfully posted. Thanks.', response.data
-		)
-
-	def test_users_cannot_add_tasks_when_error(self):
-		self.create_user('Michael', 'michael@realpython.com', 'python')
-		self.login('Michael', 'python')
-		self.app.get('tasks/', follow_redirects=True)
-		response = self.app.post('add/', data=dict(
-		    name='Go to the bank',
-		    due_date='',
-		    priority='1',
-		    posted_date='02/05/2014',
-		    status='1'
-		), follow_redirects=True)
-		self.assertIn(b'This field is required.', response.data)
-
-	def test_users_can_complete_tasks(self):
-		self.create_user('Michael', 'michael@realpython.com', 'python')
-		self.login('Michael', 'python')
-		self.app.get('tasks/', follow_redirects=True)
-		self.create_task()
-		response = self.app.get("complete/1/", follow_redirects=True)
-		self.assertIn(b'The task is complete. Nice.', response.data)
-
-	def test_users_can_delete_tasks(self):
-		self.create_user('Michael', 'michael@realpython.com', 'python')
-		self.login('Michael', 'python')
-		self.app.get('tasks/', follow_redirects=True)
-		self.create_task()
-		response = self.app.get("delete/1/", follow_redirects=True)
-		self.assertIn(b'The task was deleted.', response.data)
-
-	def test_users_cannot_complete_tasks_that_are_not_created_by_them(self):
-		self.create_user('Michael', 'michael@realpython.com', 'python')
-		self.login('Michael', 'python')
-		self.app.get('tasks/', follow_redirects=True)
-		self.create_task()
-		self.logout()
-		self.create_user('Fletcher', 'fletcher@realpython.com', 'python101')
-		self.login('Fletcher', 'python101')
-		self.app.get('tasks/', follow_redirects=True)
-		response = self.app.get("complete/1/", follow_redirects=True)
-		self.assertNotIn(
-		    b'The task is complete. Nice.', response.data
-		)
 
 if __name__ == "__main__":
 	unittest.main()
