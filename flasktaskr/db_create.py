19d1221dcf2adcaf93c4871bd52489b5622501a3
unknown <pjconnolly12@gmail.com>
2018-12-05 21:47:25 -0500

    updated files with new imports to match folder re-structure

--- flasktaskr/db_create.py
+++ flasktaskr/db_create.py
@@ -1,15 +1,24 @@
 # project/db_create.py
 
-from views import db
-from models import Task
 from datetime import date
+
+from project import db
+from project.models import Task, User
+
 
 # create the database and the db table
 db.create_all()
 
 #insert data
-#db.session.add(Task("Finish this tutorial", date(2018, 12, 7), 10, 1))
-#db.session.add(Task("Finish Real Python", date(2019, 1, 30), 10, 1))
+db.session.add(
+	User("admin", "ad@min.com", "admin", "admin")
+)
+db.session.add(
+	Task("Finish this tutorial", date(2018, 12, 10), 10, date(2018, 12, 5), 1, 1)
+)
+db.session.add(
+	Task("Finish Real Python", date(2019, 1, 31), 10, date(2018, 12, 5), 1, 1)
+)
 
 #commit the changes
 db.session.commit()
