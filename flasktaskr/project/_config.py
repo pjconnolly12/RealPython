f6a5186ed30c3700eecfeed1372a0e46c72f5750
unknown <pjconnolly12@gmail.com>
2018-12-07 12:20:52 -0500

    prepareing to deploy on heroku

--- flasktaskr/project/_config.py
+++ flasktaskr/project/_config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 CSRF_ENABLED = True
 SECRET_KEY = 'my_precious'
-DEBUG = True
+DEBUG = False
 
 DATABASE_PATH = os.path.join(basedir, DATABASE)
 
